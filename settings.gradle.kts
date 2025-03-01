724a805019e13f35ec8c11d4786cabbd1411e673
IT20664558 D.M.P.D.Daundasekara <dildaundasekara@gmail.com>
2025-03-01 12:12:12 +0530

    Project Setup

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.application") version "8.2.0" // Adjust version as needed
+        id("com.google.gms.google-services") version "4.4.2"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Hostal"
+include(":app")
+ 
